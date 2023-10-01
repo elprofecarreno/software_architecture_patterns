7a408bcf3c33087b50800473dcedcc60ee09c59e
Cristian Carreño <cristian_c_v@hotmail.com>
2023-09-30 22:59:49 -0300

    configuración volumen

--- monolithic_architecture/my_system/app/modules/admin/routes.py
+++ monolithic_architecture/my_system/app/modules/admin/routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import admin_bp
-from admin.forms import UsersForm
-from auth.models import Users
+from .forms import UsersForm
+from ..auth.models import Users
 
 
 @admin_bp.route("/")
